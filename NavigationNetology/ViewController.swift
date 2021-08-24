85813e1d0a12515eef89bfa12c092679f6fccc08
Akmal Astanov <rudy1486@mail.ru>
2021-08-24 19:21:54 +0300

    Initial Commit

---
+++ NavigationNetology/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NavigationNetology
+//
+//  Created by Akmal Astanov on 24.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
